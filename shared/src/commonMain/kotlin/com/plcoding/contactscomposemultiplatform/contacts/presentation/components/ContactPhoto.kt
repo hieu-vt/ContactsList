50c96135551a7704cf5a0aa57608951ed4344f5e
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-12-04 08:32:30 +0700

    implement data with contact list

--- shared/src/commonMain/kotlin/com/plcoding/contactscomposemultiplatform/contacts/presentation/components/ContactPhoto.kt
+++ shared/src/commonMain/kotlin/com/plcoding/contactscomposemultiplatform/contacts/presentation/components/ContactPhoto.kt
@@ -1,2 +1,16 @@
 package com.plcoding.contactscomposemultiplatform.contacts.presentation.components
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.plcoding.contactscomposemultiplatform.contacts.domain.Contact
+
+@Composable
+fun ContactPhoto(
+    contact: Contact?,
+    modifier: Modifier = Modifier,
+    iconSize: Dp = 25.dp
+){
+
+}
